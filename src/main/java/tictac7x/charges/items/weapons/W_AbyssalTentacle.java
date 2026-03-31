7e7f95afda4e208d20d6bedb6c8876ca0d4e236f
infinity <Infinitay@users.noreply.github.com>
2026-03-31 18:54:17 -0400

    fix(tent-whip): Updated check message and simplied animation condition
    - Modified the check message to also handle non-plural version "can perform 1 more attack"
    - Simplified the attack animation equipped item condition

--- src/main/java/tictac7x/charges/items/weapons/W_AbyssalTentacle.java
+++ src/main/java/tictac7x/charges/items/weapons/W_AbyssalTentacle.java
@@ -5,7 +5,6 @@ import tictac7x.charges.TicTac7xChargesImprovedConfig;
 import tictac7x.charges.item.ChargedItem;
 import tictac7x.charges.item.triggers.OnAnimationChanged;
 import tictac7x.charges.item.triggers.OnChatMessage;
-import tictac7x.charges.item.triggers.OnGraphicChanged;
 import tictac7x.charges.item.triggers.TriggerItem;
 import tictac7x.charges.store.Provider;
 import tictac7x.charges.store.ids.ItemId;
@@ -20,10 +19,10 @@ public class W_AbyssalTentacle extends ChargedItem {
 
         this.triggers.addAll(List.of(
             // Check.
-            new OnChatMessage("Your abyssal tentacle can perform (?<charges>.+) more attacks").setDynamicallyCharges(),
+            new OnChatMessage("Your abyssal tentacle can perform (?<charges>.+) more attacks?").setDynamicallyCharges(),
 
             // Attack.
-            new OnAnimationChanged(1658).itemEquipped(ItemId.ABYSSAL_TENTACLE).decreaseCharges(1),
+            new OnAnimationChanged(1658).itemEquipped().decreaseCharges(1),
 
             // Degrade
             new OnChatMessage("Your abyssal tentacle has degraded.").setFixedCharges(0)
