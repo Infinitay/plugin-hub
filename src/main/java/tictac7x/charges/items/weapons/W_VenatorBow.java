17aa85b504f6f7883c79835790d3790eb2016617
infinity <Infinitay@users.noreply.github.com>
2026-04-20 04:14:18 -0400

    refactor(venator-bow): Migrate OnWidgetLoaded triggers to use OnChatMessage
    - Migrated the OnWidgetLoaded triggers for (un)charging the bow to use OnChatMessage trigger instead

--- src/main/java/tictac7x/charges/items/weapons/W_VenatorBow.java
+++ src/main/java/tictac7x/charges/items/weapons/W_VenatorBow.java
@@ -35,51 +35,35 @@ public class W_VenatorBow extends ChargedItemWithStorage {
         );
 
         this.triggers.addAll(List.of(
-            // Charge it with ancient essence.
-            new OnWidgetLoaded(InterfaceID.OBJECTBOX, 0).onItemClick().consumer(() -> {
-                Widget objectboxTextWidget = provider.client.getWidget(InterfaceID.Objectbox.TEXT);
-                if (objectboxTextWidget != null && objectboxTextWidget.getText() != null && !objectboxTextWidget.getText().isEmpty()) {
-                    String cleanedText = TicTac7xChargesImprovedPlugin.getCleanText(objectboxTextWidget.getText());
+            // Charging the bow with essence - Check to see if the bow is already fully charged.
+            new OnChatMessage("venator bow is already fully charged.$").onItemClick().consumer(() -> {
+                final StorageItem ancientEssence = new StorageItem(ItemId.ANCIENT_ESSENCE, 50000);
+                storage.clearAndPut(ancientEssence);
+            }),
 
-                    // Adding charges
-                    // First test to see if the bow is already at max capacity
-                    if (cleanedText.endsWith("venator bow is already fully charged.")) {
-                        final StorageItem ancientEssence = new StorageItem(ItemId.ANCIENT_ESSENCE, 50000);
-                        storage.clearAndPut(ancientEssence);
-                        return;
-                    }
-
-                    // For charging your echo venator bow, as of March 2026, the game doesn't explicitly say "echo venator bow", but I'll include it just in case
-                    Pattern chargePattern = Pattern.compile("You use (?<addedCharges>.+) ancient essence to charge your (echo )?venator bow. It now has (?<charges>.+) charges.");
-                    Matcher chargeMatcher = chargePattern.matcher(cleanedText);
-                    if (chargeMatcher.find()) {
-                        int charges = TicTac7xChargesImprovedPlugin.getNumberFromCommaString(chargeMatcher.group("charges"));
-                        final StorageItem ancientEssence = new StorageItem(ItemId.ANCIENT_ESSENCE, charges);
-                        storage.clearAndPut(ancientEssence);
-                    }
-                }
+            // Charging the bow with essence - For charging your echo venator bow, as of March 2026, the game doesn't explicitly say "echo venator bow", but I'll include it just in case
+            new OnChatMessage("You use (?<addedCharges>.+) ancient essence to charge your (echo )?venator bow. It now has (?<charges>.+) charges.").onItemClick().matcherConsumer(m -> {
+                int charges = TicTac7xChargesImprovedPlugin.getNumberFromCommaString(m.group("charges"));
+                final StorageItem ancientEssence = new StorageItem(ItemId.ANCIENT_ESSENCE, charges);
+                storage.clearAndPut(ancientEssence);
             }),
 
             // Uncharge (you can only uncharge ALL charges at once)
-            new OnWidgetLoaded(InterfaceID.OBJECTBOX, 0).consumer(() -> {
-                Widget objectboxTextWidget = provider.client.getWidget(InterfaceID.Objectbox.TEXT);
-                if (objectboxTextWidget != null && objectboxTextWidget.getText() != null && !objectboxTextWidget.getText().isEmpty()) {
-                    String cleanedText = TicTac7xChargesImprovedPlugin.getCleanText(objectboxTextWidget.getText());
-                    Pattern unchargePattern = Pattern.compile("You fully uncharge your (echo )?venator bow, regaining (?<charges>.+) ancient essence in the process.");
-                    Matcher unchargeMatcher = unchargePattern.matcher(cleanedText);
+            new OnChatMessage("You fully uncharge your (echo )?venator bow, regaining (?<charges>.+) ancient essence in the process.").consumer(() -> {
+                provider.clientThread.invokeLater(() -> {
                     Widget objectboxItemWidget = provider.client.getWidget(InterfaceID.Objectbox.ITEM);
                     // Check against the uncharged item since the item displayed on the widget will be the uncharged version
-                    if (unchargeMatcher.find() && objectboxItemWidget != null && objectboxItemWidget.getItemId() == this.itemId) {
+                    if (objectboxItemWidget != null && objectboxItemWidget.getItemId() == this.itemId) {
                         storage.clear();
                     }
-                }
+                });
             }),
 
             // Check.
             new OnChatMessage("Your (echo )?venator bow has (?<charges>.+) charges? remaining.").onItemClick().matcherConsumer(m -> {
                 final StorageItem ancientEssence = new StorageItem(ItemId.ANCIENT_ESSENCE, TicTac7xChargesImprovedPlugin.getNumberFromCommaString(m.group("charges")));
                 storage.clearAndPut(ancientEssence);
-            }).setDynamicallyCharges(),
+            }),
 
             // Attack.
             new OnGraphicChanged(2289).isEquipped().consumer(() -> {
@@ -87,7 +71,7 @@ public class W_VenatorBow extends ChargedItemWithStorage {
                 if (ancientEssence.isPresent() && ancientEssence.get().getQuantity() > 0) {
                     ancientEssence.get().decreaseQuantity(1);
                 }
-            }).decreaseCharges(1)
+            })
         ));
     }
 
